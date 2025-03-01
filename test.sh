910e5eab8102f8edda66ad8be75af1e5f048c895
Deosrc <deosrc@g-de.co.uk>
2025-03-01 14:49:01 +0000

    Add arguments to test script

--- test.sh
+++ test.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
 
 ESPHOME_VERSION=latest
-WORK_DIR=./devices/localdeck
+WORK_DIR=""
 TEST_FILE=main.yaml
+
+while getopts ":d:t:v" opt; do
+  case $opt in
+    d) WORK_DIR=$OPTARG ;;
+    t) TEST_FILE=$OPTARG ;;
+    v) ESPHOME_VERSION=$OPTARG ;;
+  esac
+done
+
+if [ -z "$WORK_DIR" ]; then
+  echo "Work directory not specified." >&2
+  exit 1
+fi
+
+echo "Test Arguments:"
+echo "    Work Dir: $WORK_DIR"
+echo "    Test File: $TEST_FILE"
+echo "    ESPHome Version: $ESPHOME_VERSION"
 
 pushd "$PWD/$WORK_DIR"
 
@@ -10,7 +28,7 @@ echo "Running clean command..."
 docker run --rm -v "${PWD}":/config -it ghcr.io/esphome/esphome:$ESPHOME_VERSION clean "./tests/$TEST_FILE"
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
-  echo "Clean command failed."
+  echo "Clean command failed." >&2
   exit $RESULT
 fi
 
@@ -18,7 +36,7 @@ echo "Running compile command..."
 docker run --rm -v "${PWD}":/config -it ghcr.io/esphome/esphome:$ESPHOME_VERSION compile "./tests/$TEST_FILE"
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
-  echo "Clean command failed."
+  echo "Clean command failed." >&2
   exit $RESULT
 fi
 
