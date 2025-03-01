cb0a4755c45677d3ab90a384eeb6cced397d0924
Deosrc <deosrc@g-de.co.uk>
2025-03-01 18:37:28 +0000

    Add tests for sonoff-rf-bridge last-rf-received package

--- test.sh
+++ test.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
 
+TEST_FILE=""
 ESPHOME_VERSION=latest
-WORK_DIR=""
-TEST_FILE=main.yaml
 
-while getopts ":d:t:v:" opt; do
+while getopts ":t:v:" opt; do
   case $opt in
-    d) WORK_DIR=$OPTARG ;;
     t) TEST_FILE=$OPTARG ;;
     v) ESPHOME_VERSION=$OPTARG ;;
   esac
 done
 
-if [ -z "$WORK_DIR" ]; then
-  echo "Work directory not specified." >&2
+if [ -z "$TEST_FILE" ]; then
+  echo "Test file not specified." >&2
   exit 1
 fi
 
 echo "Test Arguments:"
-echo "    Work Dir: $WORK_DIR"
 echo "    Test File: $TEST_FILE"
 echo "    ESPHome Version: $ESPHOME_VERSION"
 
-pushd "$PWD/$WORK_DIR"
+WORK_DIR=$(dirname $(dirname "$TEST_FILE"))
+TEST_BASENAME=$(basename "$TEST_FILE")
+
+pushd "$WORK_DIR"
 
 echo "Running clean command..."
-docker run --rm -v "${PWD}":/config -t ghcr.io/esphome/esphome:$ESPHOME_VERSION clean "./tests/$TEST_FILE"
+docker run --rm -v "${PWD}":/config -t ghcr.io/esphome/esphome:$ESPHOME_VERSION clean "./tests/$TEST_BASENAME"
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
   echo "Clean command failed." >&2
@@ -33,7 +33,7 @@ if [ $RESULT -ne 0 ]; then
 fi
 
 echo "Running compile command..."
-docker run --rm -v "${PWD}":/config -t ghcr.io/esphome/esphome:$ESPHOME_VERSION compile "./tests/$TEST_FILE"
+docker run --rm -v "${PWD}":/config -t ghcr.io/esphome/esphome:$ESPHOME_VERSION compile "./tests/$TEST_BASENAME"
 RESULT=$?
 if [ $RESULT -ne 0 ]; then
   echo "Clean command failed." >&2
